3784ae9a9e280f0de833513f2db898ddb0cf91cf
Wyatt Pearsall <wyattttttt@gmail.com>
2014-08-08 08:15:36 -0700

    grid movement in place. Need to add movement speed control.

--- tiltawhirl/tiltawhirl.js
+++ tiltawhirl/tiltawhirl.js
@@ -1,15 +1,25 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoid3BlYXJzIiwiYSI6IkxubGE1VzAifQ.MvM1hTtyi5biYJ6r0dsllg';
 
-var map = new mapboxgl.Map({
-  container: 'map', // container id
-  style: 'style.json', //stylesheet location
-  center: [40, -74.50], // starting position
-  zoom: 9 // starting zoom
-});
 
 var whirl = document.getElementById("whirl");
 var stop = document.getElementById("stop");
 var fact = document.getElementById("factor");
+
+var widthThird = window.innerWidth/3;
+var heightThird= window.innerHeight/3;
+var center = [38.5,-121];
+var mouseX;
+var mouseY;
+var movementFactor = 1;
+
+
+var map = new mapboxgl.Map({
+  container: 'map', // container id
+  style: 'style.json', //stylesheet location
+  center: center, // starting position
+  zoom: 9 // starting zoom
+});
+
 
 function makeTimer(factor){
   var reqId;
@@ -19,8 +29,10 @@ function makeTimer(factor){
   if(!factor) factor = 0.25;
 
   function rotate (){
-    ready = 1;
+    ready = 1; 
     var bearing = (rotation++)*factor;
+    center = makeCenter();
+    map.setCenter(center);
     map.setBearing(bearing)
     run();
   }
@@ -55,14 +67,31 @@ function makeTimer(factor){
 }
 
 
+function updatePosition(e){
+  mouseX = e.pageX;
+  mouseY = e.pageY;
+}
+
+
+makeCenter.arr = new Array(2);
+function makeCenter(){
+  console.log(center);
+  var arr = makeCenter.arr;
+  arr[0] = center[0] + movementFactor * 0.005 * ((mouseY/heightThird>>0) - 1);
+  arr[1] = center[1] + movementFactor * 0.005 * ((mouseX/widthThird>>0) - 1) * -1;
+  return arr;
+}
+
+
 var timer = makeTimer();
 var getSpin = function(){
   timer.setSpin(parseFloat(this.value))
 }
 
-whirl.addEventListener('mousedown',timer.run);
-stop.addEventListener('mousedown',timer.stop);
-fact.addEventListener('keyup',getSpin);
 
+whirl.addEventListener('mousedown', timer.run);
+stop.addEventListener('mousedown', timer.stop);
+fact.addEventListener('keyup', getSpin);
+window.addEventListener('mousemove', updatePosition);
 
 
