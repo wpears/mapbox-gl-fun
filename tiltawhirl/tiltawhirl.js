058891e119ebe7e6c67ff88355f51d492212ccf9
Wyatt Pearsall <wyattttttt@gmail.com>
2014-08-09 21:27:22 -0700

    Movement addition

--- tiltawhirl/tiltawhirl.js
+++ tiltawhirl/tiltawhirl.js
@@ -4,6 +4,7 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoid3BlYXJzIiwiYSI6IkxubGE1VzAifQ.MvM1hTtyi5biYJ
 var whirl = document.getElementById("whirl");
 var stop = document.getElementById("stop");
 var fact = document.getElementById("factor");
+var mvFact = document.getElementById("mvfactor");
 
 var widthThird = window.innerWidth/3;
 var heightThird= window.innerHeight/3;
@@ -57,10 +58,15 @@ function makeTimer(factor){
     run();
   }    
 
+  function setMovement(mvFactor){
+    if(isNaN(mvFactor))mvFactor = movementFactor;
+      
+  }
+
 
   function stop (){
     cancelAnimationFrame(reqId);
-    ready = 1;
+    ready  1;
   }
 
   return {run:run, stop:stop, setSpin:setSpin}
@@ -77,8 +83,8 @@ makeCenter.arr = new Array(2);
 function makeCenter(){
   console.log(center);
   var arr = makeCenter.arr;
-  arr[0] = center[0] + movementFactor * 0.005 * ((mouseY/heightThird>>0) - 1);
-  arr[1] = center[1] + movementFactor * 0.005 * ((mouseX/widthThird>>0) - 1) * -1;
+  arr[0] = center[0] + movementFactor * 0.01 * ((mouseY/heightThird>>0) - 1);
+  arr[1] = center[1] + movementFactor * 0.01 * ((mouseX/widthThird>>0) - 1) * -1;
   return arr;
 }
 
@@ -86,12 +92,17 @@ function makeCenter(){
 var timer = makeTimer();
 var getSpin = function(){
   timer.setSpin(parseFloat(this.value))
-}
+};
+
+var getMovement = function(){
+  timer.setMovement(parseFloat(this.value));
+};
 
 
 whirl.addEventListener('mousedown', timer.run);
 stop.addEventListener('mousedown', timer.stop);
 fact.addEventListener('keyup', getSpin);
+mvFact.addEventListener('keyup', getMovement);
 window.addEventListener('mousemove', updatePosition);
 
 
